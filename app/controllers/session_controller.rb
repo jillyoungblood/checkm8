a635ee0a019cc166f25684dff01125a0eabc9213
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-19 19:47:16 -0400

    working on photos

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,16 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    @auth = User.where(:email => params[:email]).first
+    if @auth.present? && @auth.authenticate(params[:password])
+    else
+      session[:user_id] = nil
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+  end
 end
