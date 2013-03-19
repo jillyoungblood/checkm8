6f7c937e5b452e930dd7c2b564d07ef8b7ba0182
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-19 17:05:42 -0400

    users_spec and user_spec created

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'Users' do
+  describe 'GET /' do
+    it 'displays New Account link' do
+      visit root_path
+      page.should have_link('New Account')
+    end
+  end
+  describe 'GET /users/new' do
+    it 'displays the new user form', :js => true do
+      visit root_path
+      click_link('New Account')
+      page.should have_button('Create User')
+    end
+  end
+  describe 'JS cancel_user_form()' do
+    it 'removes the new user form', :js => true do
+      visit root_path
+      click_link('New Account')
+      click_button('Cancel')
+      page.should_not have_button('Create User')
+      page.should_not have_button('Cancel')
+    end
+  end
+  describe 'POST /users' do
+    it 'creates a new user account' do
+      visit root_path
+      click_link('New Account')
+      fill_in('Name', :with => 'Bob')
+      fill_in('Email', :with => 'bob@gmail.com')
+      fill_in('Password', :with => 'a')
+      fill_in('Password Confirmation', :with => 'a')
+      click_button('Create User')
+      page.should_not have_button('Create User')
+      expect(User.first.name).to eq 'Bob'
+    end
+  end
+end
