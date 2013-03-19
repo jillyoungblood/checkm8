3f45f59c407ddbf48aef27b75f488875ce3f8a78
Blanchet <h55nick@gmail.com>
2013-03-19 17:12:39 -0400

    AGT for models

--- spec/models/transation_spec.rb
+++ spec/models/transation_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Transaction do
-  let(:transaction) {FactoryGirl.create(:amount=>100.0, :from=>'BOA', :to=>'Chase', :dt=>'4/1/2013')}
+  let(:transaction) {FactoryGirl.create(:transaction)}
   describe 'Transaction object' do
       it 'creates a new Transaction object' do
         transation = Transaction.new
@@ -9,9 +9,9 @@ describe Transaction do
       end
     it 'creates a Transaction record' do
       expect(transaction.amount).to eq 100.0
-      expect(transaction.from).to eq 'BOA'
+      expect(transaction.from).to eq "Bank of America"
       expect(transaction.to).to eq 'Chase'
-      expect(transaction.dt).to eq '4/1/2013'
+      expect(transaction.dt).to eq '3/20/2013'
     end
   end
   describe '#user' do
