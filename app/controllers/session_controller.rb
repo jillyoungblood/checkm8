661305cd2914d42709cbfd8ddec88297aacebec4
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-20 00:43:28 -0400

    fixed stuff

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,11 @@
 class SessionController < ApplicationController
   def new
   end
-
-  def create
-    @auth = User.where(:email => params[:email]).first
-    if @auth.present? && @auth.authenticate(params[:password])
-    else
-      session[:user_id] = nil
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-
   def create
     user = User.where(:email => params[:email]).first
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
+      @banks = Bank.order(:name)
     else
       session[:user_id] = nil
     end
