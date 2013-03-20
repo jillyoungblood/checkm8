d6e25c7ba3f33f9b75ad92d6dc1d672f6f1f96eb
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-19 21:20:23 -0400

    fixed bank tests

--- spec/features/banks_spec.rb
+++ spec/features/banks_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'Banks' do
-
+l
   describe 'GET /' do
     it 'displays a list of banks' do
-      bank1 = Bank.create(name: 'Bank of America')
-      bank2 = Bank.create(name: 'HSBC')
-      bank3 = Bank.create(name: 'Citibank')
+      ['Bank of America', 'HSBC', 'Citibank'].each do |name|
+        FactoryGirl.create(:bank, :name => name)
+      end
       visit root_path
       page.should have_link('Bank of America')
       page.should have_link('HSBC')
@@ -16,10 +16,12 @@ describe 'Banks' do
 
   describe 'POST /banks' do
     it 'banks should appear alphabetically', :js => true do
+      ['Bank of America', 'HSBC', 'Citibank'].each do |name|
+        FactoryGirl.create(:bank, :name => name)
       visit root_path
       page.should have_css('table tr:nth-child(2) td:nth-child(1)', :text => 'Bank of America')
-      page.should have_css('table tr:nth-child(3) td:nth-child(1)', :text => 'HSBC')
-      page.should have_css('table tr:nth-child(4) td:nth-child(1)', :text => 'Citibank')
+      page.should have_css('table tr:nth-child(3) td:nth-child(1)', :text => 'Citibank')
+      page.should have_css('table tr:nth-child(4) td:nth-child(1)', :text => 'HSBC')
     end
   end
 
